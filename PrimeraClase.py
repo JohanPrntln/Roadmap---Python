33819316f352ae2332b4c616fafb50c8f9c4bbe2
Johan Alexander Cruz Tapasco <johan.cruz@ucp.edu.co>
2026-06-24 22:06:03 -0500

    Primera clase 24/06/2026

--- PrimeraClase.py
+++ PrimeraClase.py
@@ -1,1 +1,73 @@
-#Holaaa como estas johan
+#
+ # ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ # - Recuerda que todas las instrucciones de participación están en el
+ #   repositorio de GitHub.
+ #
+ # Lo primero... ¿Ya has elegido un lenguaje?
+ # - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ # - Este primer reto te servirá para familiarizarte con la forma de participar
+ #   enviando tus propias soluciones.
+ #
+ # EJERCICIO:
+ #
+ # ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ # debemos comenzar por el principio.
+ #
+
+ # 1)- Crea un comentario en el código y coloca la URL del sitio web oficial del
+ #https://retosdeprogramacion.com/roadmap/
+
+ #2) - Representa las diferentes sintaxis que existen de crear comentarios
+ #   en el lenguaje (en una línea, varias...).
+
+#Una linea
+
+"""
+varias 
+lineas 
+de codigo 
+"""
+# 3)- Crea una variable (y una constante si el lenguaje lo soporta).
+
+#Las constantes no existen de forma nativa en el lenguaje.
+variable1= "Primera variable"
+
+
+ #4) - Crea variables representando todos los tipos de datos primitivos
+ #   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+ # cuales son los datos primitivos del lenguaje de programcion python 
+
+# Enteros int
+#Flotantes o decimales float
+# boleanos Bool
+#Cadeans de texto string
+
+"""
+Enteros
+(-3 -2 -1 0 1 2 3)
+"""
+
+Enteros = 1
+
+"""
+Flotatnes (float) = Enteros y racionales : -3.5 -2.5 -1.5 0 1.5 2 2.5 
+"""
+Flotante = 1.5
+
+"""
+Boleanos = Verdadero o falso, y tienen un valor binario osea. true =1, false = 0
+"""
+booleanot = True
+booleanof = False
+
+"""
+Un String es variable que permite contener cadena de caracteres como letras, numeros, caracteres especiales.
+Basicamente todo lo que aparece en el codigo ASCII
+"""
+string = "Niño"
+
+ # - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print("!Hola, python¡")
+print ("¡Hola, Python")
